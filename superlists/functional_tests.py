f3f405a3462ec7daed25c9aedc0f21173d7eb58f
whatironyis <whatironyis@gmail.com>
2016-02-18 20:48:31 +0100

    now it should succed

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,23 +1,35 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
+		self.browser = webdriver.Chrome('chromedriver') #otwarcie przegladarki
+		self.browser.implicitly_wait(15)
 
-		self.browser = webdriver.Chrome('chromedriver')
-		self.browser.implicitly_wait(3)
-	#otwarcie przegladarki
 	def tearDown(self):
-		self.browser.quit()
+		self.browser.quit() #zamkniecie przegladarki
 	
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
-	#pomysl na liste rzeczy do zrobienia i otwarcie konkretnego adresu
 		self.assertIn('Listy',self.browser.title) #sprawdza czy strona ma w sobie listy
-		self.fail('Zakonczenie testu')
-#w polu tekstowym wpisuje rzeczy do zrobienia
-#klawiszem enter zatwierdza
+		#self.fail('Zakonczenie testu')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Listy',self.browser.header_text) #sprawdza czy naglowek ma listy
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Wpisz rzecz do zrobienia'
+		)
+
+		#w polu tekstowym wpisuje rzeczy do zrobienia
+		inputbox.send_keys('Kupic cos tam')
+		#klawiszem enter zatwierdza
+		inputbox.send_keys(Keys.ENTER)
+
 #1.Kupic pawie piora
 #kolejne zadanie
 #strona zostala zaktualniona i wyswietla dwie rzeczy
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -5,3 +5,4 @@ from django.http import HttpResponse
 def home_page(request):
 	return HttpResponse('<html><title>Listy rzeczy do zrobienia</title></html>')
 def about_me_page(request):
+    pass
