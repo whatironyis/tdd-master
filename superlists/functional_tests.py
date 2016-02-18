e59206ddf646647ab1b497e2204e0b2cad8f4908
whatironyis <whatironyis@gmail.com>
2016-02-18 19:37:01 +0100

    now it should succed

---
+++ superlists/functional_tests.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+	#otwarcie przegladarki
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+	#pomysl na liste rzeczy do zrobienia i otwarcie konkretnego adresu
+		self.assertIn('Listy',self.browser.title) #sprawdza czy strona ma w sobie listy
+		self.fail('Zakonczenie testu')
+#w polu tekstowym wpisuje rzeczy do zrobienia
+#klawiszem enter zatwierdza
+#1.Kupic pawie piora
+#kolejne zadanie
+#strona zostala zaktualniona i wyswietla dwie rzeczy
+#sprawdzenie czy strona zapamietuje te rzeczy
+#jeszcze raz wchodzi na strone
+#udalo sie idziemy spac
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
